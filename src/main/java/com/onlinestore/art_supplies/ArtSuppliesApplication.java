2a61c72ddb03903cd0fcc347bff9eb7ab247b03d
ciubotarum <ciubotarum@outlook.com>
2024-11-01 13:10:07 +0200

    Online shop for art supplies

---
+++ src/main/java/com/onlinestore/art_supplies/ArtSuppliesApplication.java
@@ -0,0 +1,13 @@
+package com.onlinestore.art_supplies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSuppliesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtSuppliesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlinestore/art_supplies/ArtSuppliesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinestore.art_supplies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtSuppliesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
