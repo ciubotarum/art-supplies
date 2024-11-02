cb483d6ee62130a3dcd5bb0f5a6bfc57f7856571
ciubotarum <ciubotarum@outlook.com>
2024-11-02 16:19:04 +0200

    Create a user table

---
+++ src/main/java/com/onlinestore/art_supplies/model/User.java
@@ -0,0 +1,24 @@
+package com.onlinestore.art_supplies.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name")
+    private String fullName;
+    private String email;
+    private String phone;
+}
