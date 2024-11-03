d1e1ab14de1331acdc2607a209914b32536e52ad
ciubotarum <ciubotarum@outlook.com>
2024-11-03 12:28:23 +0200

    Add a product controller

---
+++ src/main/java/com/onlinestore/art_supplies/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.onlinestore.art_supplies.controller;
+
+import com.onlinestore.art_supplies.model.Product;
+import com.onlinestore.art_supplies.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = service.getProductById(id);
+
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product) {
+        try {
+            System.out.println(product);
+            Product product1 = service.addProduct(product);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
