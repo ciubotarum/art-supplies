72362192e2720e875d4c4d001dd77411b0d74909
ciubotarum <ciubotarum@outlook.com>
2024-11-03 12:16:06 +0200

    Add a product class

---
+++ src/main/java/com/onlinestore/art_supplies/model/Product.java
@@ -0,0 +1,27 @@
+package com.onlinestore.art_supplies.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
+    private String category;
+    private String image;
+}
