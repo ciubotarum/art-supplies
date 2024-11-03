e1fb1bc8da7c9e24d735c2683d293fd3fcd4f37c
ciubotarum <ciubotarum@outlook.com>
2024-11-03 12:27:58 +0200

    Add a product repository

---
+++ src/main/java/com/onlinestore/art_supplies/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.onlinestore.art_supplies.repository;
+
+import com.onlinestore.art_supplies.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p from Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(String keyword);
+}
