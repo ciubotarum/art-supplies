ee9b0190f72349d3f6454c85c8ff0160b4fd3314
ciubotarum <ciubotarum@outlook.com>
2024-11-05 14:00:43 +0200

    Add a user repository

---
+++ src/main/java/com/onlinestore/art_supplies/users/UserRepository.java
@@ -0,0 +1,10 @@
+package com.onlinestore.art_supplies.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+    User getByUsername(String username);
+}
