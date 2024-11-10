fcf6a35802df512e3a2716a26afe39b8f5febfc3
ciubotarum <ciubotarum@outlook.com>
2024-11-10 16:51:38 +0200

    Create mappings to get all users or a user by id

--- src/main/java/com/onlinestore/art_supplies/users/UserController.java
+++ src/main/java/com/onlinestore/art_supplies/users/UserController.java
@@ -6,25 +6,52 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/users")
 public class UserController { ;
+    private final UserService userService;
+
     @Autowired
-    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody Map<String, String> payload) {
-        return new ResponseEntity<>(userService.register(payload.get("username"), payload.get("password")), HttpStatus.CREATED);
+    public ResponseEntity<User> register(@RequestBody User user) {
+        return new ResponseEntity<>(userService.register(user), HttpStatus.CREATED);
     }
     @PostMapping("/login")
-    public ResponseEntity<User> login(@RequestBody Map<String, String> payload) {
-        return new ResponseEntity<>(userService.login(payload.get("username"), payload.get("password")), HttpStatus.OK);
+    public ResponseEntity<String> login(@RequestParam String username, @RequestParam String password) {
+        User user = userService.login(username, password);
+        if (user != null) {
+            return ResponseEntity.ok("Login successful! Welcome, " + user.getFullName());
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password.");
+        }
     }
-    @GetMapping
-    public ResponseEntity<List<User>> getUsers() {
-        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getAllUsers(@PathVariable Long userId, @RequestParam Long adminId) {
+        User adminUser = userService.getUserById(adminId).orElse(null);
+        if (adminUser != null && Boolean.TRUE.equals(adminUser.getIsAdmin())) {
+            User user = userService.getUserById(userId).orElse(null);
+            return ResponseEntity.ok(user);
+        }  else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied: Only admins can view all users.");
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllUsers(@RequestParam Long adminId) {
+        User adminUser = userService.getUserById(adminId).orElse(null);
+
+        if (adminUser != null && Boolean.TRUE.equals(adminUser.getIsAdmin())) {
+            List<User> users = userService.getAllUsers();
+            return ResponseEntity.ok(users);
+        }  else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied: Only admins can view all users.");
+        }
     }
 }
