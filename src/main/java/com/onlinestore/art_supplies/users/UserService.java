116690eaf770d6f682da94aa6077c71ae2373d12
ciubotarum <ciubotarum@outlook.com>
2024-11-10 16:50:45 +0200

    Add methods to get all users or one user by id

--- src/main/java/com/onlinestore/art_supplies/users/UserService.java
+++ src/main/java/com/onlinestore/art_supplies/users/UserService.java
@@ -3,38 +3,44 @@ package com.onlinestore.art_supplies.users;
 import ch.qos.logback.core.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-    public User register(String username, String password) {
-        if (StringUtil.isNullOrEmpty(username) || StringUtil.isNullOrEmpty(password)) {
+    public User register(User user) {
+        if (StringUtil.isNullOrEmpty(user.getUsername()) || StringUtil.isNullOrEmpty(user.getPassword())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Empty username or password");
         }
-        if (userRepository.existsByUsername(username)) {
+        if (userRepository.existsByUsername(user.getUsername())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This username already exists");
         }
-        String encodedPassword = passwordEncoder.encode(password);
-        User user = new User(username, encodedPassword);
+        if (user.getIsAdmin() == null) {
+            user.setIsAdmin(false);
+        }
         return userRepository.save(user);
     }
 
     public User login(String username, String password) {
         User user = userRepository.getByUsername(username);
-        if (user == null || !passwordEncoder.matches(password, user.getPassword())) {
+        if (user == null || !password.matches(user.getPassword())) {
             throw new RuntimeException("Invalid username or password");
         }
         return user;
+    }
+
+    public Optional<User> getUserById(Long userId) {
+        return userRepository.findById(userId);
     }
 
     public List<User> getAllUsers() {
