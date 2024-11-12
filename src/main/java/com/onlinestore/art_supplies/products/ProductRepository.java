a2e3ec5f5afd21c27a3d4664bbb887970c07bd1d
ciubotarum <ciubotarum@outlook.com>
2024-11-12 11:01:17 +0200

    Add search query

--- src/main/java/com/onlinestore/art_supplies/products/ProductRepository.java
+++ src/main/java/com/onlinestore/art_supplies/products/ProductRepository.java
@@ -1,14 +1,18 @@
 package com.onlinestore.art_supplies.products;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query("SELECT p from Product p WHERE " +
-            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    @Query("SELECT p FROM Product p " +
+            "JOIN p.category c " + // Join with the Category entity
+            "WHERE LOWER(p.productName) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(c.categoryName) LIKE LOWER(CONCAT('%', :keyword, '%'))")
     List<Product> searchProducts(String keyword);
 }
