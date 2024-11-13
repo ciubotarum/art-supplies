102387f1a7bc6d223edd9f7207c21c33a25d1c7f
ciubotarum <ciubotarum@outlook.com>
2024-11-13 11:08:41 +0200

    Add an order item entity

---
+++ src/main/java/com/onlinestore/art_supplies/order/orderitem/OrderItem.java
@@ -0,0 +1,33 @@
+package com.onlinestore.art_supplies.order.orderitem;
+
+import com.onlinestore.art_supplies.order.Order;
+import com.onlinestore.art_supplies.products.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+}
