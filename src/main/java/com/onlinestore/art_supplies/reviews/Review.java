a5a8dea0a254b644e970358327473e6261f32eed
ciubotarum <ciubotarum@outlook.com>
2024-11-17 12:58:42 +0200

    Add annotations to represent relations between tables

--- src/main/java/com/onlinestore/art_supplies/reviews/Review.java
+++ src/main/java/com/onlinestore/art_supplies/reviews/Review.java
@@ -1,5 +1,7 @@
 package com.onlinestore.art_supplies.reviews;
 
+import com.onlinestore.art_supplies.products.Product;
+import com.onlinestore.art_supplies.users.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,13 +20,13 @@ public class Review {
     @Column(name = "review_id")
     private Long reviewId;
 
-    @Column(name = "product_id")
-    private Long productId;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 
-    @Column(name = "user_id")
-    private Long userId;
-
-    private int rating;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @Column(name = "review_text")
     private String reviewText;
@@ -32,7 +34,4 @@ public class Review {
     @Column(name = "created_at")
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    public Review(String body) {
-        this.reviewText = body;
-    }
 }
