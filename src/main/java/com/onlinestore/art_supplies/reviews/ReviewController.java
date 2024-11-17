fe3410f0c704f3a7acec4293be89b61334034c07
ciubotarum <ciubotarum@outlook.com>
2024-11-17 12:59:49 +0200

    Add mapping to get all the reviews

--- src/main/java/com/onlinestore/art_supplies/reviews/ReviewController.java
+++ src/main/java/com/onlinestore/art_supplies/reviews/ReviewController.java
@@ -1,5 +1,6 @@
 package com.onlinestore.art_supplies.reviews;
 
+import com.onlinestore.art_supplies.dto.ReviewRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,20 +11,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/reviews")
 public class ReviewController {
+    private final ReviewService reviewService;
+
     @Autowired
-    private ReviewService reviewService;
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
 
     @PostMapping
-    public ResponseEntity<Review> createReview(@RequestBody Review review) {
-        Review  createdReview = reviewService.createReview(review);
+    public ResponseEntity<Review> createReview(@RequestBody ReviewRequest reviewRequest, @RequestParam String username) {
+        Review  createdReview = reviewService.createReview(reviewRequest.getReviewText(), reviewRequest.getProductId(), username);
         return ResponseEntity.ok(createdReview);
     }
 
     @DeleteMapping("/{reviewId}")
-    public ResponseEntity<String> deleteReview(@RequestBody Long reviewId) {
+    public ResponseEntity<String> deleteReview(@PathVariable Long reviewId) {
         try {
             reviewService.deleteReview(reviewId);
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Review deleted successfully!");
+            return ResponseEntity.ok("Review with id " + reviewId + " was deleted successfully!");
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
@@ -32,6 +37,12 @@ public class ReviewController {
     @GetMapping("/product/{productId}")
     public ResponseEntity<List<Review>> getReviewsByProductId(@PathVariable Long productId) {
         List<Review> reviews = reviewService.getReviewsByProductId(productId);
+        return ResponseEntity.ok(reviews);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Review>> getAllReviews() {
+        List<Review> reviews = reviewService.getAllReviews();
         return ResponseEntity.ok(reviews);
     }
 }
