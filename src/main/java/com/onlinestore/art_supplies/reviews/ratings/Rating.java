25db10b9dc594a413862c94757611b6682f37fe0
ciubotarum <ciubotarum@outlook.com>
2024-11-17 13:00:40 +0200

    Add a rating entity

---
+++ src/main/java/com/onlinestore/art_supplies/reviews/ratings/Rating.java
@@ -0,0 +1,30 @@
+package com.onlinestore.art_supplies.reviews.ratings;
+
+import com.onlinestore.art_supplies.products.Product;
+import com.onlinestore.art_supplies.users.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ratings")
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rating_id")
+    private Long ratingId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer rating;
+}
