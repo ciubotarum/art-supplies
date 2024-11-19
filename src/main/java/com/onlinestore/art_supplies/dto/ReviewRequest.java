cc90fab23dd8f1270df0b1ba2a46d334606a257f
ciubotarum <ciubotarum@outlook.com>
2024-11-19 14:39:23 +0200

    Add validations for review text and product id to not be null

--- src/main/java/com/onlinestore/art_supplies/dto/ReviewRequest.java
+++ src/main/java/com/onlinestore/art_supplies/dto/ReviewRequest.java
@@ -1,5 +1,7 @@
 package com.onlinestore.art_supplies.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReviewRequest {
+    @NotBlank(message = "Review text cannot be blank.")
     private String reviewText;
+
+    @NotNull(message = "Product ID cannot be null.")
     private Long productId;
 }
