b03ce85031b36e6a65fe2414e19af49565b302ae
ciubotarum <ciubotarum@outlook.com>
2024-11-20 12:21:24 +0200

    Add a method to extract users if ordered products

--- src/main/java/com/onlinestore/art_supplies/order/OrderRepository.java
+++ src/main/java/com/onlinestore/art_supplies/order/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.onlinestore.art_supplies.order;
 
+import com.onlinestore.art_supplies.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUser_UserId(Long userId);
+
+    Boolean existsByUserAndOrderItems_Product_ProductId(User user, Long productId);
+
 }
