899869dfd711a0525edd248c7eb8c1599a1c776a
ciubotarum <ciubotarum@outlook.com>
2024-11-25 16:48:42 +0200

    Add tests for RatingController class

---
+++ src/test/java/com/onlinestore/art_supplies/ratings/RatingControllerTest.java
@@ -0,0 +1,155 @@
+package com.onlinestore.art_supplies.ratings;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@WebMvcTest(RatingController.class)
+class RatingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private RatingService ratingService;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testCreateRating_Success() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/ratings")
+                        .contentType("application/json")
+                        .content("""
+                                {
+                                  "ratingValue": 5, 
+                                  "productId": 1,
+                                  "username": "testUser"
+                                 }
+                                """))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void testCreateRating_UserNotFound() throws Exception {
+
+        when(ratingService.createRating(anyInt(), anyLong(), anyString()))
+                .thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with username: testUser"));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/ratings")
+                        .contentType("application/json")
+                        .content("""
+                                {
+                                  "ratingValue": 5, 
+                                  "productId": 1,
+                                  "username": "testUser"
+                                 }
+                                """))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.status().reason("User not found with username: testUser"));
+    }
+
+    @Test
+    void testCreateRating_ProductNotFound() throws Exception {
+
+        when(ratingService.createRating(anyInt(), anyLong(), anyString()))
+                .thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found with id: 1"));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/ratings")
+                        .contentType("application/json")
+                        .content("""
+                                {
+                                  "ratingValue": 5, 
+                                  "productId": 1,
+                                  "username": "testUser"
+                                 }
+                                """))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.status().reason("Product not found with id: 1"));
+    }
+
+    @Test
+    void testCreateRating_UserHasNotOrderedProduct() throws Exception {
+
+        when(ratingService.createRating(anyInt(), anyLong(), anyString()))
+                .thenThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST, "User has not ordered this product."));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/ratings")
+                        .contentType("application/json")
+                        .content("""
+                                {
+                                  "ratingValue": 5, 
+                                  "productId": 1,
+                                  "username": "testUser"
+                                 }
+                                """))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.status().reason("User has not ordered this product."));
+    }
+
+    @Test
+    void testCreateRating_ValidationError() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/ratings")
+                        .contentType("application/json")
+                        .content("{}"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void testGetRatingsByProductId() throws Exception {
+        Rating rating1 = new Rating();
+        Rating rating2 = new Rating();
+        when(ratingService.getRatingsByProductId(1L)).thenReturn(List.of(rating1, rating2));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/ratings/product/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(2));
+
+    }
+
+    @Test
+    void testGetRatingsByProductId_InvalidProductId() throws Exception {
+        when(ratingService.getRatingsByProductId(1L))
+                .thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "No ratings found for product with id: 1"));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/ratings/product/1"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.status().reason("No ratings found for product with id: 1"));
+
+    }
+
+    @Test
+    void testGetAllRatings() throws Exception {
+        Rating rating1 = new Rating();
+        Rating rating2 = new Rating();
+        when(ratingService.getAllRatings()).thenReturn(List.of(rating1, rating2));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/ratings/all"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(2));
+    }
+
+    @Test
+    void testGetAllRatings_NoRatingsFound() throws Exception {
+        when(ratingService.getAllRatings()).thenReturn(List.of());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/ratings/all"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(0));
+    }
+}
