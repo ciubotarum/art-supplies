bd6e7708d49f50a9de93bbe0833f52ad92ff4a24
ciubotarum <ciubotarum@outlook.com>
2024-12-01 19:41:48 +0200

    Add new cases to test when users are logged in and admins

--- src/test/java/com/onlinestore/art_supplies/users/UserControllerTest.java
+++ src/test/java/com/onlinestore/art_supplies/users/UserControllerTest.java
@@ -14,11 +14,10 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
@@ -36,7 +35,6 @@ class UserControllerTest {
 
     @Test
     void register_ShouldReturnCreatedStatus_WhenUserIsValid() throws Exception {
-
         mockMvc.perform(post("/users/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
@@ -65,7 +63,7 @@ class UserControllerTest {
                                     "fullName": "New User",
                                     "email": "",
                                     "phone": "1234567890",
-                                    "isAdmin": false,
+                                    "isAdmin": false
                                 }
                                 """))
                 .andExpect(status().isBadRequest());
@@ -78,7 +76,7 @@ class UserControllerTest {
         user.setPassword("password");
         user.setFullName("User FullName");
 
-        when(userService.login("user", "password")).thenReturn(user);
+        when(userService.login("user", "password")).thenReturn(Optional.of(user));
 
         mockMvc.perform(post("/users/login")
                         .param("username", "user")
@@ -89,13 +87,43 @@ class UserControllerTest {
 
     @Test
     void login_ShouldReturnUnauthorizedStatus_WhenCredentialsAreInvalid() throws Exception {
-        when(userService.login("invalidUser", "invalidPassword")).thenThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED));
+        when(userService.login("invalidUser", "invalidPassword")).thenReturn(Optional.empty());
 
         mockMvc.perform(post("/users/login")
                         .param("username", "invalidUser")
                         .param("password", "invalidPassword"))
                 .andExpect(status().isUnauthorized())
-                .andExpect(content().string("Invalid username or password."));
+                .andExpect(content().string("Invalid username or password"));
+    }
+
+    @Test
+    void getUserById_ShouldReturnOkStatus_WhenAdminUser() throws Exception {
+        User adminUser = new User();
+        adminUser.setIsAdmin(true);
+
+        when(userService.getUserById(1L)).thenReturn(Optional.of(adminUser));
+        when(userService.isLoggedIn(1L)).thenReturn(true);
+        when(userService.getUserById(2L)).thenReturn(Optional.of(new User()));
+
+        mockMvc.perform(get("/users/2")
+                        .param("adminId", "1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getUserById_ShouldReturnForbiddenStatus_WhenNotAdminUser() throws Exception {
+        User nonAdminUser = new User();
+
+        when(userService.getUserById(1L)).thenReturn(Optional.of(nonAdminUser));
+        when(userService.isLoggedIn(1L)).thenReturn(true);
+
+        doThrow(new ResponseStatusException(HttpStatus.FORBIDDEN, "Access denied: Only logged-in admins can perform this action."))
+                .when(userService).checkAdminAndLoggedIn(1L);
+
+        mockMvc.perform(get("/users/2")
+                        .param("adminId", "1"))
+                .andExpect(status().isForbidden())
+                .andExpect(content().string("Access denied: Only logged-in admins can perform this action."));
     }
 
     @Test
@@ -103,21 +131,29 @@ class UserControllerTest {
         User adminUser = new User();
         adminUser.setIsAdmin(true);
 
-        when(userService.getUserById(1L)).thenReturn(Optional.of(adminUser));
+        when(userService.isLoggedIn(1L)).thenReturn(true);
+        doNothing().when(userService).checkAdminAndLoggedIn(1L);
         when(userService.getAllUsers()).thenReturn(List.of(new User(), new User()));
 
         mockMvc.perform(get("/users/all")
                         .param("adminId", "1"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
     }
 
     @Test
     void getAllUsers_ShouldReturnForbiddenStatus_WhenNotAdminUser() throws Exception {
-        when(userService.getUserById(1L)).thenReturn(Optional.of(new User()));
+        User nonAdminUser = new User();
+
+        when(userService.getUserById(1L)).thenReturn(Optional.of(nonAdminUser));
+        when(userService.isLoggedIn(1L)).thenReturn(true);
+
+        doThrow(new ResponseStatusException(HttpStatus.FORBIDDEN, "Access denied: Only logged-in admins can perform this action."))
+                .when(userService).checkAdminAndLoggedIn(1L);
 
         mockMvc.perform(get("/users/all")
                         .param("adminId", "1"))
                 .andExpect(status().isForbidden())
-                .andExpect(content().string("Access denied: Only admins can view all users."));
+                .andExpect(content().string("Access denied: Only logged-in admins can perform this action."));
     }
 }
