68c116486c126c08b8ae324b561da2d7a6c72887
ciubotarum <ciubotarum@outlook.com>
2024-12-02 14:16:18 +0200

    Add annotation for swagger documentation

--- src/main/java/com/onlinestore/art_supplies/products/ProductController.java
+++ src/main/java/com/onlinestore/art_supplies/products/ProductController.java
@@ -2,10 +2,10 @@ package com.onlinestore.art_supplies.products;
 
 import com.onlinestore.art_supplies.category.Category;
 import com.onlinestore.art_supplies.category.CategoryService;
-import com.onlinestore.art_supplies.users.User;
 import com.onlinestore.art_supplies.users.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@Tag(name = "Product Controller", description = "Operations related to products")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -29,7 +30,7 @@ public class ProductController {
 
     @GetMapping("/all")
     @Operation(summary = "Get all products",
-            description = "Get all products from the database",
+            description = "Retrieve all products from the database",
             responses = {
                     @ApiResponse(responseCode = "200", description = "Products found")
             })
@@ -39,48 +40,47 @@ public class ProductController {
 
     @GetMapping("/product/{id}")
     @Operation(summary = "Get product by ID",
-            description = "Get a product by its ID",
+            description = "Retrieve a product by its ID",
+
             responses = {
                     @ApiResponse(responseCode = "200", description = "Product found"),
                     @ApiResponse(responseCode = "404", description = "Product not found")
             })
-    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+    public ResponseEntity<?> getProductById(@PathVariable Long id) {
         Product product = productService.getProductById(id);
         if (product != null) {
             return new ResponseEntity<>(product, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping
     @Operation(summary = "Add a new product",
             description = "Add a new product to the database",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Put productName, description, price, quantity, image"),
             responses = {
                     @ApiResponse(responseCode = "200", description = "Product added"),
-                    @ApiResponse(responseCode = "400", description = "Category not found"),
-                    @ApiResponse(responseCode = "403", description = "Forbidden for non-admin users")
+                    @ApiResponse(responseCode = "404", description = "Category not found, or user not found"),
+                    @ApiResponse(responseCode = "403", description = "Forbidden for non-admin users"),
+
             })
     public ResponseEntity<?> addProduct(
             @RequestBody @Valid Product product,
             @RequestParam Long adminId,
             @RequestParam Long categoryId) {
 
-        User adminUser = userService.getUserById(adminId).orElse(null);
+        userService.checkAdminAndLoggedIn(adminId);
 
-        if (adminUser != null && Boolean.TRUE.equals(adminUser.getIsAdmin())) {
-            Category category = categoryService.getCategoryById(categoryId);
-            if (category == null) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category not found");
-            }
-
-            product.setCategory(category);
-            Product savedProduct = productService.addProduct(product);
-            return ResponseEntity.ok(savedProduct);
-        } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                    .body("Access denied: Only admins can add a product.");
+        Category category = categoryService.getCategoryById(categoryId);
+        if (category == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category not found");
         }
+
+        product.setCategory(category);
+        Product savedProduct = productService.addProduct(product);
+        return ResponseEntity.ok(savedProduct);
     }
 
 
@@ -89,14 +89,12 @@ public class ProductController {
             description = "Delete a product by ID",
             responses = {
                     @ApiResponse(responseCode = "200", description = "Product deleted"),
-                    @ApiResponse(responseCode = "404", description = "Product not found"),
+                    @ApiResponse(responseCode = "404", description = "Product not found or user not found"),
                     @ApiResponse(responseCode = "403", description = "Forbidden for non-admin users")
             })
     public ResponseEntity<?> deleteProduct(@RequestParam Long productId, @RequestParam Long adminId) {
-        User adminUser = userService.getUserById(adminId).orElse(null);
-        if (adminUser  == null || !Boolean.TRUE.equals(adminUser .getIsAdmin())) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Only admin can delete a product.");
-        }
+        userService.checkAdminAndLoggedIn(adminId);
+
         if (!productService.productExistsById(productId)) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No product found with the given ID.");
         }
@@ -118,15 +116,19 @@ public class ProductController {
     @PutMapping("/{productId}")
     @Operation(summary = "Update product",
             description = "Update a product by ID",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Put productName, description, price, quantity, image, categoryId"),
             responses = {
                     @ApiResponse(responseCode = "200", description = "Product updated"),
-                    @ApiResponse(responseCode = "404", description = "Product not found"),
-                    @ApiResponse(responseCode = "403", description = "Forbidden")
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "Product not found or user not found"),
+                    @ApiResponse(responseCode = "403", description = "Forbidden for non-admin users")
             })
     public ResponseEntity<?> updateProduct(@PathVariable Long productId, @RequestParam Long adminId, @RequestBody @Valid Product updatedProduct) {
-        User adminUser = userService.getUserById(adminId).orElse(null);
-        if (adminUser  == null || !Boolean.TRUE.equals(adminUser .getIsAdmin())) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Only admin can delete a product.");
+        userService.checkAdminAndLoggedIn(adminId);
+
+        if (!productService.productExistsById(productId)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No product found with the given ID.");
         }
         Product updated = productService.updateProduct(productId, updatedProduct);
         return ResponseEntity.ok(updated);
@@ -139,10 +141,10 @@ public class ProductController {
                     @ApiResponse(responseCode = "200", description = "Products found"),
                     @ApiResponse(responseCode = "404", description = "No such category found")
             })
-    public ResponseEntity<List<Product>> filterProductsByType(@RequestParam String categoryName) {
+    public ResponseEntity<?> filterProductsByCategory(@RequestParam String categoryName) {
         List<Product> products = productService.getProductsByCategoryName(categoryName);
         if (products.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No such category");
         }
         return ResponseEntity.ok(products);
     }
