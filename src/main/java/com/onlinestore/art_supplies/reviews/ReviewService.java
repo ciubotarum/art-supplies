ddc75a0b49c2e9334146e788586c2a76720298d0
ciubotarum <ciubotarum@outlook.com>
2024-12-08 20:48:37 +0200

    Change methods to check if a user is logged in

--- src/main/java/com/onlinestore/art_supplies/reviews/ReviewService.java
+++ src/main/java/com/onlinestore/art_supplies/reviews/ReviewService.java
@@ -5,6 +5,7 @@ import com.onlinestore.art_supplies.products.Product;
 import com.onlinestore.art_supplies.products.ProductRepository;
 import com.onlinestore.art_supplies.users.User;
 import com.onlinestore.art_supplies.users.UserRepository;
+import com.onlinestore.art_supplies.users.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,19 +18,25 @@ public class ReviewService {
     private final OrderRepository orderRepository;
     private final ProductRepository productRepository;
     private final UserRepository userRepository;
+    private final UserService userService;
 
-    public ReviewService(ReviewRepository reviewRepository, OrderRepository orderRepository, ProductRepository productRepository, UserRepository userRepository) {
+    public ReviewService(ReviewRepository reviewRepository, OrderRepository orderRepository, ProductRepository productRepository, UserRepository userRepository, UserService userService) {
         this.reviewRepository = reviewRepository;
         this.orderRepository = orderRepository;
         this.productRepository = productRepository;
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     public Review createReview(String reviewText, Long productId, String username) {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with username: " + username));
+        if (!userService.isLoggedIn(user.getUserId())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User is not logged in.");
+        }
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found with id: " + productId));
+
         if (userHasOrderedProduct(user, productId)) {
             Review review = new Review();
             review.setReviewText(reviewText);
@@ -41,12 +48,24 @@ public class ReviewService {
         }
     }
 
-    public void deleteReview(Long reviewId) {
-        if (reviewRepository.existsById(reviewId)) {
-            reviewRepository.deleteById(reviewId);
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Review with " + reviewId + " does not exist.");
+    public void deleteReview(Long reviewId, Long userId) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Review with " + reviewId + " does not exist."));
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User with ID " + userId + " does not exist."));
+
+        if (!userService.isLoggedIn(userId)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User is not logged in.");
         }
+        try {
+            userService.checkAdminAndLoggedIn(userId);
+        } catch (ResponseStatusException e) {
+            if (!review.getUser().getUserId().equals(userId)) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User is not authorized to delete this review.");
+            }
+        }
+        reviewRepository.deleteById(reviewId);
     }
 
     public List<Review> getReviewsByProductId(Long productId) {
