07c23a41ef2ed7955cc3e60fc4cc0718a6d13f83
ciubotarum <ciubotarum@outlook.com>
2024-12-19 16:39:31 +0200

    Update versions in pom.xml

--- src/main/java/com/onlinestore/art_supplies/order/OrderRepository.java
+++ src/main/java/com/onlinestore/art_supplies/order/OrderRepository.java
@@ -2,9 +2,11 @@ package com.onlinestore.art_supplies.order;
 
 import com.onlinestore.art_supplies.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUser_UserId(Long userId);
 
--- src/main/java/com/onlinestore/art_supplies/order/orderitem/OrderItemRepository.java
+++ src/main/java/com/onlinestore/art_supplies/order/orderitem/OrderItemRepository.java
@@ -2,9 +2,11 @@ package com.onlinestore.art_supplies.order.orderitem;
 
 import com.onlinestore.art_supplies.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     List<OrderItem> findByOrder(Order order);
 }
