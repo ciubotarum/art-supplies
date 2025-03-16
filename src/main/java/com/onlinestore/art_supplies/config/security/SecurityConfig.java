7fbfcd1fc7a61beaa724da691cc37bcde4b2b852
ciubotarum <ciubotarum@outlook.com>
2025-03-16 22:43:03 +0200

    Add a security configuration

---
+++ src/main/java/com/onlinestore/art_supplies/config/security/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.onlinestore.art_supplies.config.security;
+
+import com.onlinestore.art_supplies.users.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticationFilter filter;
+
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, CustomAuthenticationFilter filter) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.filter = filter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("users/register", "users/login", "/swagger-ui/**", "/v3/api-docs/**")
+                        .permitAll()
+                        .anyRequest().authenticated())  // all requests must be authenticated
+//        .formLogin(Customizer.withDefaults())  // use form login for browser
+//                .httpBasic(Customizer.withDefaults())   // work good and in postman, we can disable one of this or both but won't have the login form
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);  // don't create session
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());    // use BCryptPasswordEncoder
+        provider.setUserDetailsService(customUserDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
