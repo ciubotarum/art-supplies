310f3cd06e6627b6b23ec46f732ae760030e87c6
ciubotarum <ciubotarum@outlook.com>
2025-04-10 00:32:59 +0300

    Create cart repositories

---
+++ src/main/java/com/onlinestore/art_supplies/order/cart/CartItemRepository.java
@@ -0,0 +1,12 @@
+package com.onlinestore.art_supplies.order.cart;
+
+import com.onlinestore.art_supplies.products.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findByCartAndProduct(Cart cart, Product product);
+}
