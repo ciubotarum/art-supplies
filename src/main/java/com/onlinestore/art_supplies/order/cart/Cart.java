fed1bbdc46c1fa220c3985d2e8ab625aa78c0e83
ciubotarum <ciubotarum@outlook.com>
2025-04-10 00:32:09 +0300

    Add models for cart

---
+++ src/main/java/com/onlinestore/art_supplies/order/cart/Cart.java
@@ -0,0 +1,29 @@
+package com.onlinestore.art_supplies.order.cart;
+
+import com.onlinestore.art_supplies.users.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", unique = true)
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+}
---
+++ src/main/java/com/onlinestore/art_supplies/order/cart/CartItem.java
@@ -0,0 +1,29 @@
+package com.onlinestore.art_supplies.order.cart;
+
+import com.onlinestore.art_supplies.products.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+}
